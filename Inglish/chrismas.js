f58f860bb0bddf946d9541ee15d61f2c748088fe
AimarKarrik <71828910+AimarKarrik@users.noreply.github.com>
2021-12-15 10:57:37 +0200

    Create chrismas.js

---
+++ Inglish/chrismas.js
@@ -0,0 +1,23 @@
+const people = {
+    aHamsterWhoIsReallyAFilmstar,
+    aHunchback,
+    aFeministPrincess,
+    aTalkingHousePlant,
+    aPolitician,
+    aBankRobber
+}
+const objects = {
+    aMagicWalkman,
+    aTelephoneLineToYourFairyGodmother,
+    aKey,
+    aWashingMachineThatCanRInseOutAllYourTroubles,
+    aFlyingCar,
+    aRing,
+    treasure,
+    aSpell,
+    aBottomlesBankAccount,
+    aWindow
+}
+const location = {
+    aLake
+}
